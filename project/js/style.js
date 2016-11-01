8a933d468a80c3cb18dad4ccbc8d7d016bfb1bfe
eirikedh <eirik.kopperud.edh@gmail.com>
2016-11-01 16:30:12 +0100

    Update style.js

--- project/js/style.js
+++ project/js/style.js
@@ -1,5 +1,11 @@
 
-function buttonclick() {
+function buttonclick1() {
   window.location.href="om_oss.html";
+  
+  }
+
+
+function buttonclick2() {
+  window.location.href="vare_produkter.html";
   
   }
