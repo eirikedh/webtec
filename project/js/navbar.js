242a1cf2f3fc385c9b6ec681c7e8e950524f16e8
kronen77 <even__kj@hotmail.com>
2016-11-06 15:21:58 +0100

    Update navbar.js

--- project/js/navbar.js
+++ project/js/navbar.js
@@ -6,58 +6,58 @@ window.onload = function addNavBar(){
   for (let i = 1; i<6; i++){
     if (i = 1){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "hjem.html";
-      navlink.appendChild(document.createTextNode("Hjem"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "hjem.html";
+      nava.appendChild(document.createTextNode("Hjem"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 2){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "vare_produkter.html";
-      navlink.appendChild(document.createTextNode("Våre Produkter"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "vare_produkter.html";
+      nava.appendChild(document.createTextNode("Våre Produkter"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 3){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "thflor.html";
-      navlink.appendChild(document.createTextNode("TH Flor"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "thflor.html";
+      nava.appendChild(document.createTextNode("TH Flor"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 4){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "gallerihtml";
-      navlink.appendChild(document.createTextNode("Galleri"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "gallerihtml";
+      nava.appendChild(document.createTextNode("Galleri"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 5){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "tips_og_triks.html";
-      navlink.appendChild(document.createTextNode("Tips og triks"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "tips_og_triks.html";
+      nava.appendChild(document.createTextNode("Tips og triks"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 6){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "om_oss.html";
-      navlink.appendChild(document.createTextNode("Om oss"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "om_oss.html";
+      nava.appendChild(document.createTextNode("Om oss"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
     if (i = 7){
       let li = document.createElement("li");
-      let navlink = document.createElement("a");
-      navlink.href = "kontakt.html";
-      navlink.appendChild(document.createTextNode("Kontakt"));
-      li.appendChild(navlink)
+      let nava = document.createElement("a");
+      nava.href = "kontakt.html";
+      nava.appendChild(document.createTextNode("Kontakt"));
+      li.appendChild(nava)
       nav.appendChild(li)
     }
   }
