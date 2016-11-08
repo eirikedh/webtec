dbcefdfc2c262749e69ba2f8ee0de2e0213052be
Tvedt1 <tvedtenb@gmail.com>
2016-11-08 15:08:51 +0100

    Create slideshow.js

---
+++ project/js/slideshow.js
@@ -0,0 +1,14 @@
+let bilder = 0;
+loop();
+
+function loop() {
+    let i;
+    let x = document.getElementsByClassName("slideshow_auto");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    bilder++;
+    if (bilder > x.length) {bilder = 1}
+    x[bilder-1].style.display = "block";
+    setTimeout(loop, 4000);
+}
