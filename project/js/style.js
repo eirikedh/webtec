80afe1c61f6150ba61367038a949cc72fe5249f8
eirikedh <eirik.kopperud.edh@gmail.com>
2016-11-08 12:43:03 +0100

    Update style.js

--- project/js/style.js
+++ project/js/style.js
@@ -1,21 +1,20 @@
 
 function buttonclick1() {
-  window.location.href="om_oss.html";
-  
-  }
-
+	window.location.href="om_oss.html";
+  	}
 
 function buttonclick2() {
-  window.location.href="vare_produkter.html";
-  
-  }
-
-
+	window.location.href="vare_produkter.html";
+  	}
 
 function buttonclick3() {
-  window.location.href="galleri.html";
-  
-  }
+	window.location.href="galleri.html";
+	}
+
+function dropdown() {
+	document.getElementById("myDropdown").classList.toggle("show");
+	}
+
 
   var navMenu = document.getElementById("nav");
   
