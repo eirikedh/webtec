1e78d2317906ad48439765e41b5d0ab2dad2c1b3
kronen77 <even__kj@hotmail.com>
2016-11-08 14:44:12 +0100

    Update navbar.js

--- project/js/navbar.js
+++ project/js/navbar.js
@@ -44,3 +44,57 @@ navbar("om_oss.html", "Om Oss")
 navbar("kontakt.html", "Kontak")
 
 navdiv.appendChild(nav)
+
+function createListElement(link,text, liste){
+  let ul = document.getElementById(liste)
+  let li = document.createElement("li")
+  let a = document.createElement("a")
+  a.href = link
+  a.appendChild(document.createTextNode(text))
+  li.appendChild(a)
+  ul.appendChild(li)
+}
+
+function createSpecialList(){
+  let endaul = document.getElementById("map2ul");
+  let produktli = document.createElement("li")
+  produktli.id = "produkter"
+  produktli.appendChild(document.createTextNode("Våre Produkter"))
+  endaul.appendChild(produktli)
+  let nyli = document.createElement("li")
+  endaul.appendChild(nyli)
+  let nyul = document.createElement("ul")
+  nyli.appendChild(nyul)
+  let li = document.createElement("li")
+  let a = document.createElement("a")
+  a.href = "vare_produkter.html"
+  a.appendChild(document.createTextNode("Løn Gartneri"))
+  li.appendChild(a)
+  nyul.appendChild(li)
+  let THli = document.createElement("li")
+  let THa = document.createElement("a")
+  THa.href = "thflor_produkter"
+  THa.appendChild(document.createTextNode("TH FLor"))
+  THli.appendChild(THa)
+  nyul.appendChild(THli)
+}
+
+function createImage(){
+  let imgdiv = document.getElementById("logofooterdiv")
+  let image = document.createElement("img")
+  image.src = "img/logo2.png"
+  image.alt = "Løn Gartneri logo"
+  image.id = "footerlogo"
+  imgdiv.appendChild(image)
+}
+createImage()
+
+createListElement("hjem.html","Hjem", "map1ul");
+createListElement("galleri.html","Galleri", "map1ul")
+createListElement("om_oss.html","Om Oss", "map1ul")
+
+createSpecialList()
+
+createListElement("thflor.html","Hjem", "map3ul");
+createListElement("kontakt.html","Kontakt", "map3ul")
+createListElement("tips_og_triks.html","Tips og Triks", "map3ul")
