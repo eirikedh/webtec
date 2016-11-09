d673c9ec2a43b8ccb48fac85237737af8c496828
eirikedh <eirik.kopperud.edh@gmail.com>
2016-11-09 19:38:34 +0100

    Update galleri.js

--- project/js/galleri.js
+++ project/js/galleri.js
@@ -1,1 +1,20 @@
 
+
+function checkSeason() {
+  if  (document.getElementById("sommer").checked == true)  {
+    let autumn = document.getElementsByClassName("autumn");
+    for (i = 0; i < autumn.length; i++) {
+      autumn[i].style.display = 'none';
+      }
+    let vinter = document.getElementsByClassName("vinter");
+    for (i = 0; i < vinter.length; i++) {
+      vinter[i].style.display = 'none';
+      }
+    let spring = document.getElementsByClassName("spring");
+    for (i = 0; i < spring.length; i++) {
+      spring[i].style.display = 'none';
+      }
+  }
+
+
+}
