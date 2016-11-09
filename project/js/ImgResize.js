fe31ae42ec8e24a6b2b362861fe01f8caa3e7371
kronen77 <even__kj@hotmail.com>
2016-11-09 10:26:22 +0100

    Add files via upload

---
+++ project/js/ImgResize.js
@@ -0,0 +1,19 @@
+function imageResize(bilde){
+  let cover = document.getElementById("overlay");
+  cover.style.display = "inline-block"
+  bilde.style.width = "80%"
+  bilde.style.height = "80%"
+  bilde.style.position = "fixed";
+  bilde.style.zIndex = 10000;
+  bilde.style.top = "80px";
+  bilde.style.left = "200px";
+
+  cover.addEventListener("click", function (){
+    cover.style.display = "none";
+    bilde.style.position ="static"
+    bilde.style.float = "left"
+    bilde.style.width = "180px"
+    bilde.style.height ="150px"
+    bilde.style.marginBottom = "18px"
+  })
+}
