4b5e6ded37c4baab0d72d543978a439f967d4230
eirikedh <eirik.kopperud.edh@gmail.com>
2016-11-10 09:50:07 +0100

    Update galleri.js

--- project/js/galleri.js
+++ project/js/galleri.js
@@ -22,7 +22,59 @@ function checkSeason() {
       season3[i].style.display = 'none';
       }
     }
+  
+  
+  /* HVIS KUN HØST ER VALG */
+  if  (høst.checked == true)  {
+    let season1 = document.getElementsByClassName("summer");
+    for (i = 0; i < season1.length; i++) {
+      season1[i].style.display = 'none';
+      }
+    let season2 = document.getElementsByClassName("vinter");
+    for (i = 0; i < season2.length; i++) {
+      season2[i].style.display = 'none';
+      }
+    let season3 = document.getElementsByClassName("spring");
+    for (i = 0; i < season3.length; i++) {
+      season3[i].style.display = 'none';
+      }
+    }
 
+
+  /* HVIS KUN VINTER ER VALG */
+  if  (vinter.checked == true)  {
+    let season1 = document.getElementsByClassName("summer");
+    for (i = 0; i < season1.length; i++) {
+      season1[i].style.display = 'none';
+      }
+    let season2 = document.getElementsByClassName("autumn");
+    for (i = 0; i < season2.length; i++) {
+      season2[i].style.display = 'none';
+      }
+    let season3 = document.getElementsByClassName("spring");
+    for (i = 0; i < season3.length; i++) {
+      season3[i].style.display = 'none';
+      }
+    }
+
+
+  /* HVIS KUN VÅR ER VALG */
+  if  (vår.checked == true)  {
+    let season1 = document.getElementsByClassName("summer");
+    for (i = 0; i < season1.length; i++) {
+      season1[i].style.display = 'none';
+      }
+    let season2 = document.getElementsByClassName("vinter");
+    for (i = 0; i < season2.length; i++) {
+      season2[i].style.display = 'none';
+      }
+    let season3 = document.getElementsByClassName("autumn");
+    for (i = 0; i < season3.length; i++) {
+      season3[i].style.display = 'none';
+      }
+    }
+
+  
 /*
   if  (sommer.checked == true && høst.checked == true)  {
     let season1 = document.getElementsByClassName("spring");
@@ -57,22 +109,6 @@ function checkSeason() {
       }
     }
 */
-
-  /* HVIS KUN HØST ER VALG */
-  if  (høst.checked == true)  {
-    let season1 = document.getElementsByClassName("summer");
-    for (i = 0; i < season1.length; i++) {
-      season1[i].style.display = 'none';
-      }
-    let season2 = document.getElementsByClassName("vinter");
-    for (i = 0; i < season2.length; i++) {
-      season2[i].style.display = 'none';
-      }
-    let season3 = document.getElementsByClassName("spring");
-    for (i = 0; i < season3.length; i++) {
-      season3[i].style.display = 'none';
-      }
-    }
 
 
   /* HVIS INGEN SESONGER ER VALGT: VISES ALLE BLOMSTER */
