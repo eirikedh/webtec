6f920ded9a33f3e592b07aeced9949ce1c3cca47
Kirit2 <erlingmoan@gmail.com>
2016-11-10 21:13:56 +0100

    Update style.js

--- project/js/style.js
+++ project/js/style.js
@@ -62,8 +62,8 @@ nav.onmouseover = function(e) {
 
 function mouseOut(e) {
 	var remove = true;
-	if (e.toElement.parentElement != null) {
-		if (e.toElement.parentElement.className == "dropdown-content show") {
+	if (e.relatedTarget.parentElement != null) {
+		if (e.relatedTarget.parentElement.className == "dropdown-content show") {
 			remove = false;
 		}
 	}
