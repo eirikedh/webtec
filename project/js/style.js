cd454e954bfc938a6416e9cd81213c43aa2f89a6
eirikedh <eirik.kopperud.edh@gmail.com>
2016-11-10 18:43:18 +0100

    Update style.js

--- project/js/style.js
+++ project/js/style.js
@@ -1,5 +1,7 @@
 
 /* -------------------- HJEMMESIDE-BUTTONS ---------------------- */
+
+/* Sender brukeren til en bestemt url-side */
 function buttonclick1() {
 	window.location.href="om_oss.html";
   	}
@@ -15,12 +17,12 @@ function buttonclick3() {
 
 /* -------------------- THFLOR-DROPDOWN ---------------------- */
 
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+/* Når åpningstider-knappen klikkes, enten vises eller skjules dropdownen */
 function openTimes() {
 	document.getElementById("myOpentime").classList.toggle("show");
 	}
 
-/* Close the dropdown if the user clicks outside of it */
+/* Lukke dropdownen hvis man klikker utenfor */
 window.onclick = function(event) {
 	if (!event.target.matches('.open')) {
 		let dropdowns = document.getElementsByClassName("open-content");
@@ -37,43 +39,45 @@ window.onclick = function(event) {
 
 /* -------------------- NAVBAR DROPDOWN ---------------------- */
 
-  var navMenu = document.getElementById("nav");
-  
-  nav.onmouseover = function(e) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var d = 0; d < dropdowns.length; d++) {
-      var openDropdown = dropdowns[d];
-      if (!openDropdown.classList.contains('show')) {
-        if (e.target.className == "dropbtn") {
-          openDropdown.classList.add('show');
-          openDropdown.addEventListener("mouseout", mouseOut);
-        }
-      }
-      else {
-        if (e.target.className != "dropbtn" && e.target.parentElement.className != "dropdown-content show") {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+var navMenu = document.getElementById("nav");
+
+nav.onmouseover = function(e) {
+	var dropdowns = document.getElementsByClassName("dropdown-content");
+	for (var d = 0; d < dropdowns.length; d++) {
+		var openDropdown = dropdowns[d];
+		if (!openDropdown.classList.contains('show')) {
+			if (e.target.className == "dropbtn") {
+  				openDropdown.classList.add('show');
+  				openDropdown.addEventListener("mouseout", mouseOut);
+			}
+		}
+		
+		else {
+			if (e.target.className != "dropbtn" && e.target.parentElement.className != "dropdown-content show") {
+  				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+function mouseOut(e) {
+	var remove = true;
+	if (e.toElement.parentElement != null) {
+		if (e.toElement.parentElement.className == "dropdown-content show") {
+			remove = false;
+		}
+	}
 	
-  function mouseOut(e) {
-  	var remove = true;
-    if (e.toElement.parentElement != null) {
-    	if (e.toElement.parentElement.className == "dropdown-content show") {
-        remove = false;
-      }
-    }
-  	if (remove) {
-    		var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var d = 0; d < dropdowns.length; d++) {
-          var openDropdown = dropdowns[d];
-          if (openDropdown.classList.contains('show')) {
-          	openDropdown.classList.remove('show');
-            openDropdown.removeEventListener("mouseout", mouseOut);
-          }
-        }
-    }
-  	
-  }
+	if (remove) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		for (var d = 0; d < dropdowns.length; d++) {
+  			var openDropdown = dropdowns[d];
+  			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+    				openDropdown.removeEventListener("mouseout", mouseOut);
+ 			}
+		}
+	}
+
+}
 
