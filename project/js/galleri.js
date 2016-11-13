f3caeab3b4b1bb79b84687807b64abfe9abe36c5
kronen77 <even__kj@hotmail.com>
2016-11-13 13:59:48 +0100

    Update galleri.js

--- project/js/galleri.js
+++ project/js/galleri.js
@@ -1,14 +1,14 @@
 function checkSeason(whichstring, whichnumber){
-  let season = document.getElementById(whichstring)
-  let seasonnumber = document.getElementsByClassName(whichnumber)
-    if  (season.checked == true){
+  let season = document.getElementById(whichstring) //Henter id fra sesong i checkboxen
+  let seasonnumber = document.getElementsByClassName(whichnumber) //For å kunne endre alle bildene i samme class
+    if  (season.checked == true){ //Sjekker om man har filtret for å se bildene i denne sesongen
       for (i = 0; i < seasonnumber.length; i++) {
-        seasonnumber[i].style.display = 'block';
+        seasonnumber[i].style.display = 'block';  // Går gjennom alle bildene i classen seasonnumber og setter display block
         }
       }
-    if (season.checked == false  ){
+    if (season.checked == false  ){ //Sjekker om season ikker er checked
       for (j = 0; j < seasonnumber.length; j++) {
-        seasonnumber[j].style.display = 'none';
+        seasonnumber[j].style.display = 'none'; //Går gjennom alle bildene i classen og setter display none
         }
       }
 }
